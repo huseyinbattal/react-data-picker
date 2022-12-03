0cad73c1ac52ac39a7f92aa6b5d9303137479afe
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-12-03 22:45:33 +0300

    data-picker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import DatePicker from 'react-datepicker';
 import './App.css';
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from 'react';
+
 
 function App() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+    setShow(true);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+
+      <DatePicker
+      selected={startDate}
+      onChange={onChange}
+      startDate={startDate}
+      endDate={endDate}
+      selectsRange
+        inline
+      />
+
+      {show ? <p><div className='start'>Start Date : {JSON.stringify(startDate.getDate())}.{JSON.stringify(startDate.getMonth() + 1)}.{JSON.stringify(startDate.getFullYear())}</div><hr style={{width:"350px"}}/><div className='end'>{endDate?"End Date : " : ""} {endDate ? JSON.stringify(endDate.getDate()) : ""}{ endDate?".":""}{endDate ? JSON.stringify(endDate.getMonth() + 1) : ""}{ endDate?".":""}{endDate?JSON.stringify(endDate.getFullYear()):""}</div></p>:""}
+
     </div>
   );
 }
